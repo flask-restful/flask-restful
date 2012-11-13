8b21444ac1224d6128eb0502909ec792acf43e78
Guillaume BINET <gbin@gootz.net>
2012-11-13 10:48:01 -0800

    small PEP8 violation and a better parameter name

--- tests/test_api.py
+++ tests/test_api.py
@@ -233,7 +233,7 @@ class APITestCase(unittest.TestCase):
 
     def test_output_json_error(self):
         app = Flask(__name__)
-        flask_restful.Api(app, output_json_errors=True)
+        flask_restful.Api(app, output_errors=True)
         app = app.test_client()
         resp = app.get("/foo")
         self.assertEquals(resp.status_code, 404)
@@ -241,7 +241,7 @@ class APITestCase(unittest.TestCase):
 
     def test_output_standard_error(self):
         app = Flask(__name__)
-        flask_restful.Api(app, output_json_errors=False)
+        flask_restful.Api(app, output_errors=False)
         app = app.test_client()
         resp = app.get("/foo")
         self.assertEquals(resp.status_code, 404)
