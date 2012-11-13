81f716dbdcc96ad70a806e541ff0f72f9288c6e8
Guillaume BINET <gbin@gootz.net>
2012-11-13 10:48:01 -0800

    small PEP8 violation and a better parameter name

--- flask_restful/__init__.py
+++ flask_restful/__init__.py
@@ -40,14 +40,14 @@ class Api(object):
     """
 
     def __init__(self, app, prefix='', default_mediatype='application/json',
-                 decorators=None, output_json_errors = True):
+                 decorators=None, output_errors=True):
         self.representations = dict(DEFAULT_REPRESENTATIONS)
         self.urls = {}
         self.prefix = prefix
         self.app = app
         self.default_mediatype = default_mediatype
         self.decorators = decorators if decorators else []
-        if output_json_errors and hasattr(app, 'handle_exception'):
+        if output_errors and hasattr(app, 'handle_exception'):
             self.original_handle_exception, app.handle_exception = app.handle_exception, self.handle_exception
             self.original_handle_user_exception, app.handle_user_exception = app.handle_user_exception, self.handle_user_exception
 
