3fdf077127745be6a67beeb596c9bd7387a7d405
Guillaume BINET <gbin@gootz.net>
2012-11-14 14:27:26 -0800

    corrected a metaclass bug in Resource

--- flask_restful/__init__.py
+++ flask_restful/__init__.py
@@ -217,7 +217,7 @@ class Api(object):
 
 class ResourceMetaClass(MethodViewType):
     def __new__(mcs, classname, bases, classDict):
-        new_class = type.__new__(mcs, classname, bases, classDict)
+        new_class = MethodViewType.__new__(mcs, classname, bases, classDict)
         if classname != 'Resource':
             methods = {}
             for verb in ('get', 'put', 'post', 'head', 'delete'):
