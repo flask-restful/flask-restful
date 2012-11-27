d4a1ba23c8d153d23f9f2ddd29aff89e25095e81
Guillaume BINET <gbin@gootz.net>
2012-11-27 09:59:05 -0800

    test coverage of the linking and embedding features

---
+++ tests/test_hal.py
@@ -0,0 +1,134 @@
+import unittest
+from flask import Flask
+from flask.ext.restful import LinkedResource, Api, Embed
+
+#noinspection PyUnresolvedReferences
+from nose.tools import assert_equals # you need it for tests in form of continuations
+from flask.ext.restful.declarative import Verb, parameters, output, link
+from flask.ext.restful.fields import Integer, String
+
+class HALTestCase(unittest.TestCase):
+    def test_standalone_linked_resource(self):
+        class Foo(LinkedResource):
+            _self = '/foo'
+
+            @Verb(parameters(),
+                  output(test_out=Integer))
+            def get(self):
+                return output(test_out=42)
+
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_root(Foo)
+
+        app = app.test_client()
+        resp = app.get("/foo")
+        self.assertEquals(resp.status_code, 200)
+        self.assertEquals(resp.data, '{"test_out": 42}')
+
+    def test_parameterized_linked_resource(self):
+        class Foo(LinkedResource):
+            _self = '/foo/{FOO_ID}'
+
+            @Verb(parameters(),
+                  output(Some_output=String),
+                  link())
+            def get(self, FOO_ID=None):
+                return output(Some_output="This is my ID : [%s]" % FOO_ID)
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_root(Foo)
+
+        app = app.test_client()
+        resp = app.get("/foo/314")
+        self.assertEquals(resp.status_code, 200)
+        self.assertEquals(resp.data, '{"_links": {"self": {"href": "/foo/314"}}, "Some_output": "This is my ID : [314]"}')
+
+    def test_simple_linked_resource(self):
+        class Foo(LinkedResource):
+            _self = '/bar/foo'
+
+            @Verb(parameters(),
+                  output())
+            def get(self):
+                return output()
+
+        class Bar(LinkedResource):
+            _self = '/bar'
+
+            @Verb(parameters(),
+                  output(),
+                  link(My_dear_foo=Foo))
+            def get(self):
+                return output(test_out=42)
+
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_root(Bar)
+
+        app = app.test_client()
+        resp = app.get("/bar")
+        self.assertEquals(resp.status_code, 200)
+        self.assertEquals(resp.data, '{"_links": {"self": {"href": "/bar"}, "My_dear_foo": {"href": "/bar/foo"}}}')
+
+
+    def test_simple_embedded_linked_resource(self):
+        class Foo(LinkedResource):
+            _self = '/bar/foo'
+
+            @Verb(parameters(),
+                  output(Some_output=String))
+            def get(self):
+                return output(Some_output="Should be in the response directly")
+
+        class Bar(LinkedResource):
+            _self = '/bar'
+
+            @Verb(parameters(),
+                  output(My_dear_foo=Foo),
+                  link())
+            def get(self):
+                return output(My_dear_foo=Embed(Foo))
+
+
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_root(Bar)
+
+        app = app.test_client()
+        resp = app.get("/bar")
+        self.assertEquals(resp.status_code, 200)
+        self.assertEquals(resp.data, '{"_links": {"self": {"href": "/bar"}}, "_embedded": {"My_dear_foo": {"Some_output": "Should be in the response directly"}}}')
+
+    def test_parameterized_embedded_linked_resource(self):
+        class Foo(LinkedResource):
+            _self = '/bar/{FOO_ID}'
+
+            @Verb(parameters(),
+                  output(Some_output=String),
+                  link())
+            def get(self, FOO_ID=None):
+                return output(Some_output="This is my ID : [%s]" % FOO_ID)
+
+        class Bar(LinkedResource):
+            _self = '/bar'
+
+            @Verb(parameters(),
+                  output(My_dear_foo=Foo),
+                  link())
+            def get(self):
+                return output(My_dear_foo=Embed(Foo, {"FOO_ID": 42}))
+
+
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_root(Bar)
+
+        app = app.test_client()
+        resp = app.get("/bar")
+        self.assertEquals(resp.status_code, 200)
+        self.assertEquals(resp.data, '{"_links": {"self": {"href": "/bar"}}, "_embedded": {"My_dear_foo": {"_links": {"self": {"href": "/bar/42"}}, "Some_output": "This is my ID : [42]"}}}')
+
+
+if __name__ == '__main__':
+    unittest.main()
