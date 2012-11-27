4d24714b103d7cc3aacbd9b1198a1f2f78e5ff38
Guillaume BINET <gbin@gootz.net>
2012-11-27 11:07:46 -0800

    another not nice regression tested while improving code coverage

--- flask_restful/links.py
+++ flask_restful/links.py
@@ -7,26 +7,12 @@ class Link(object):
         self.params = params
 
     @property
-    def href(self):
-        uri = self.linked_resource._self
-        if self.params: # TODO: basic brutal impl handle the other cases
-            uri = uri.format(**self.params) # FIXME it is probably not even secure
-        return uri
-
-    @property
     def title(self):
         return self._title
 
     @property
     def templated(self):
         return self.params is not None
-
-    def to_dict(self):
-        answer = {}
-        if self.title:
-            answer['title'] = self.title
-        answer['href'] = self.href
-        return answer
 
 class Embed(object):
     def __init__(self, linked_resource_class, get_parameters = None):
