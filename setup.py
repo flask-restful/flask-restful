7f7f3b17ffab5ea342e81537b9f845223fc3f5a5
Guillaume BINET <gbin@gootz.net>
2012-12-17 12:51:04 -0800

    bumped revision

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-RESTful',
-    version='0.1.2',
+    version='0.1.3',
     url='https://www.github.com/twilio/flask-restful/',
     author='Kyle Conroy',
     author_email='kyle@twilio.com',
