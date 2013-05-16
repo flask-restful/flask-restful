e8f050f258363d40ac56d868f591cbe9db3a6152
Guillaume BINET <gbin@gootz.net>
2013-05-16 09:42:34 -0700

    added basic test coverage of out embedded ordered dict (backported from the official cpython tests for 2.7+)

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -1,5 +1,5 @@
 import unittest
-from flask_restful.utils.crypto import encrypt, decrypt
+from flask_restful.utils.crypto import encrypt, decrypt, create_cipher
 
 
 class CryptoTestCase(unittest.TestCase):
@@ -8,3 +8,11 @@ class CryptoTestCase(unittest.TestCase):
         seed = 'deadbeefcafebabe'
         message = 'It should go through'
         self.assertEqual(decrypt(encrypt(message, key, seed), key, seed), message)
+
+    def test_parameters_error_case(self):
+        key = '0123456789abcdef0123456789abcdef'
+        seed = 'deadbeefcafebabe'
+        create_cipher(key, seed)  # should pass ok
+
+        self.assertRaises(ValueError, lambda: create_cipher(key, seed[:-1]))
+        self.assertRaises(ValueError, lambda: create_cipher(key[:-1], seed))
