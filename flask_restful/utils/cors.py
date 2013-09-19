080bae8c55b51f3aefd1af028307fcb41a525704
Doug Black <dblack@twilio.com>
2013-09-19 11:57:52 -0700

    remove basestring reference

--- flask_restful/utils/cors.py
+++ flask_restful/utils/cors.py
@@ -10,9 +10,9 @@ def crossdomain(origin=None, methods=None, headers=None,
     """
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
+    if headers is not None and not isinstance(headers, str):
         headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
+    if not isinstance(origin, str):
         origin = ', '.join(origin)
     if isinstance(max_age, timedelta):
         max_age = max_age.total_seconds()
