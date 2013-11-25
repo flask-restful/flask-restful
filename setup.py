b3fe4fb5ad76658dfec9ae1de32bf2def2d7acf7
Sam Kimbrel <skimbrel@twilio.com>
2013-11-25 09:51:37 -0800

    Bump to version 0.2.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Flask-RESTful',
-    version='0.2.7',
+    version='0.2.8',
     url='https://www.github.com/twilio/flask-restful/',
     author='Kyle Conroy',
     author_email='help@twilio.com',
