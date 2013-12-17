075e8a2be55708a1332f51e11c5d3f579ecb0fc0
Kevin Burke <kevin@twilio.com>
2013-12-17 14:14:59 -0800

    Add positive type. Update natural type. Add tests

--- Makefile
+++ Makefile
@@ -24,7 +24,7 @@ test-install: install
 		--download-cache /tmp/pipcache --use-mirrors
 
 test:
-	. venv/bin/activate; nosetests --with-coverage --cover-package=flask.ext.restful
+	. venv/bin/activate; nosetests --with-coverage --cover-package=flask_restful
 
 analysis:
 	. venv/bin/activate; flake8 --select=E112,E113,E901,E902,W601,W602,W603,W604,W402,W403,W404,W802,W803,W804,W805,W806 --ignore=W801 flask_restful tests
--- tests/test_types.py
+++ tests/test_types.py
@@ -2,7 +2,7 @@ import unittest
 from flask_restful import types
 import datetime
 #noinspection PyUnresolvedReferences
-from nose.tools import assert_equals # you need it for tests in form of continuations
+from nose.tools import assert_equal, assert_raises # you need it for tests in form of continuations
 import six
 
 # http://docs.python.org/library/datetime.html?highlight=datetime#datetime.tzinfo.fromutc
@@ -31,7 +31,7 @@ def test_datetime_formatters():
                   "Sat, 01 Jan 2011 23:59:59 -0000"),
         ]
     for date_obj, expected in dates:
-        yield assert_equals, types.rfc822(date_obj), expected
+        yield assert_equal, types.rfc822(date_obj), expected
 
 def test_urls():
     urls = [
@@ -54,14 +54,14 @@ def test_urls():
         ]
 
     for value in urls:
-        yield assert_equals, types.url(value), value
+        yield assert_equal, types.url(value), value
 
 def check_bad_url_raises(value):
     try:
         types.url(value)
         assert False, "shouldn't get here"
     except ValueError as e:
-        assert_equals(six.text_type(e), u"{0} is not a valid URL".format(value))
+        assert_equal(six.text_type(e), u"{0} is not a valid URL".format(value))
 
 def test_bad_urls():
     values = [
@@ -100,51 +100,65 @@ def check_url_error_message(value):
         types.url(value)
         assert False, u"types.url({0}) should raise an exception".format(value)
     except ValueError as e:
-        assert_equals(six.text_type(e), 
+        assert_equal(six.text_type(e),
                       (u"{0} is not a valid URL. Did you mean: http://{0}".format(value)))
 
 
 class TypesTestCase(unittest.TestCase):
 
     def test_boolean_false(self):
-        self.assertEquals(types.boolean("False"), False)
+        assert_equal(types.boolean("False"), False)
 
 
     def test_boolean_true(self):
-        self.assertEquals(types.boolean("true"), True)
+        assert_equal(types.boolean("true"), True)
 
 
     def test_boolean_upper_case(self):
-        self.assertEquals(types.boolean("FaLSE"), False)
+        assert_equal(types.boolean("FaLSE"), False)
 
 
     def test_boolean(self):
-        self.assertEquals(types.boolean("FaLSE"), False)
+        assert_equal(types.boolean("FaLSE"), False)
+
+
+    def test_bad_boolean(self):
+        assert_raises(ValueError, lambda: types.boolean("blah"))
 
 
     def test_date_later_than_1900(self):
-        self.assertEquals(types.date("1900-01-01"), datetime.datetime(1900, 1, 1))
+        assert_equal(types.date("1900-01-01"), datetime.datetime(1900, 1, 1))
 
 
     def test_date_too_early(self):
-        self.assertRaises(ValueError, lambda: types.date("0001-01-01"))
+        assert_raises(ValueError, lambda: types.date("0001-01-01"))
 
 
     def test_date_input_error(self):
-        self.assertRaises(ValueError, lambda: types.date("2008-13-13"))
+        assert_raises(ValueError, lambda: types.date("2008-13-13"))
 
     def test_date_input(self):
-        self.assertEquals(types.date("2008-08-01"), datetime.datetime(2008, 8, 1))
+        assert_equal(types.date("2008-08-01"), datetime.datetime(2008, 8, 1))
 
     def test_natual_negative(self):
-        self.assertRaises(ValueError, lambda: types.natural(-1))
+        assert_raises(ValueError, lambda: types.natural(-1))
 
-    def test_natual(self):
-        self.assertEquals(3, types.natural(3))
+    def test_natural(self):
+        assert_equal(3, types.natural(3))
 
 
     def test_natual_string(self):
-        self.assertRaises(ValueError, lambda: types.natural('foo'))
+        assert_raises(ValueError, lambda: types.natural('foo'))
+
+    def test_positive(self):
+        assert_equal(1, types.positive(1))
+        assert_equal(10000, types.positive(10000))
+
+    def test_positive_zero(self):
+        assert_raises(ValueError, lambda: types.positive(0))
+
+    def test_positive_negative_input(self):
+        assert_raises(ValueError, lambda: types.positive(-1))
 
 
 if __name__ == '__main__':
