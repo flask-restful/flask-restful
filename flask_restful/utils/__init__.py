22ca58ecfefead5a2e770c64e1df9c5258288f28
Xabier Eizmendi <xeizmendi@gmail.com>
2019-10-21 12:45:34 +0200

    Remove unused import

--- flask_restful/utils/__init__.py
+++ flask_restful/utils/__init__.py
@@ -3,10 +3,7 @@ import sys
 try:
     from collections.abc import OrderedDict
 except ImportError:
-    try:
-        from collections import OrderedDict
-    except ImportError:
-        from ordereddict import OrderedDict
+    from collections import OrderedDict
 
 from werkzeug.http import HTTP_STATUS_CODES
 
