e64023f47c7500257affb73af77fef51dbbd3b1b
Mayank Arya <mayankarya2906@gmail.com>
2020-10-15 12:46:06 +0530

    remove unused depencencies

--- flask_restful/fields.py
+++ flask_restful/fields.py
@@ -1,6 +1,4 @@
-from datetime import datetime
 from calendar import timegm
-import pytz
 from decimal import Decimal as MyDecimal, ROUND_HALF_EVEN
 from email.utils import formatdate
 import six
@@ -9,8 +7,7 @@ try:
 except ImportError:
     # python3
     from urllib.parse import urlparse, urlunparse
-
-from flask_restful import inputs, marshal
+from flask_restful import marshal
 from flask import url_for, request
 
 __all__ = ["String", "FormattedString", "Url", "DateTime", "Float",
