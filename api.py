629096b7f99b381e070a2b2d89b0403ff2dff347
lidkicker <github@lidkicker.com>
2020-12-01 12:57:33 -0500

    first commit

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
