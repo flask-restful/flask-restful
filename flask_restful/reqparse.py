ffa15e89b4276991e94f72d6a4ed4a1e190727ff
Jonathan Woytek <woytek@cert.org>
2022-03-30 16:42:06 -0400

    fix for #936

--- flask_restful/reqparse.py
+++ flask_restful/reqparse.py
@@ -122,6 +122,8 @@ class Argument(object):
         else:
             values = MultiDict()
             for l in self.location:
+                if not request.is_json and l == 'json':
+                    continue
                 value = getattr(request, l, None)
                 if callable(value):
                     value = value()
