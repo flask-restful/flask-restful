9f22849541a455a23ef8efe26e87d6bb50d4d8cc
HomiGrotas <CryingBaby@YourDad.com>
2023-02-07 18:30:26 +0200

    pep8

--- examples/todo.py
+++ examples/todo.py
@@ -10,16 +10,22 @@ TODOS = {
     'todo3': {'task': 'profit!'},
 }
 
+
 def abort_if_todo_doesnt_exist(todo_id):
     if todo_id not in TODOS:
         abort(404, message="Todo {} doesn't exist".format(todo_id))
 
+
 parser = reqparse.RequestParser()
 parser.add_argument('task')
+
 
 # Todo
 #   show a single todo item and lets you delete them
 class Todo(Resource):
+    """
+
+    """
     def get(self, todo_id):
         abort_if_todo_doesnt_exist(todo_id)
         return TODOS[todo_id]
@@ -48,9 +54,10 @@ class TodoList(Resource):
         TODOS[todo_id] = {'task': args['task']}
         return TODOS[todo_id], 201
 
-##
-## Actually setup the Api resource routing here
-##
+
+#
+# Actually setup the Api resource routing here
+#
 api.add_resource(TodoList, '/todos')
 api.add_resource(Todo, '/todos/<string:todo_id>')
 
